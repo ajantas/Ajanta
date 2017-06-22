4cecb7d60c557e72dbc38528bc8ca257b60f654b
Ajantas <ajantas71@gmail.com>
2017-06-22 20:24:33 +0530

    Server trial

---
+++ lab-3-server/src/main/java/com/lab_3_server/App.java
@@ -0,0 +1,14 @@
+package com.lab_3_server;
+
+/**
+ * Hello world!
+ *
+ */
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
